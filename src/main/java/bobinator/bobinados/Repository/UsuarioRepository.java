369797de546bf4a68ea3a27d8680d05d6c1f4bd7
Federico Nickler <fedenickler@gmail>
2022-04-17 17:10:10 -0300

    Agrego Login y Registro

--- src/main/java/bobinator/bobinados/Repository/UsuarioRepository.java
+++ src/main/java/bobinator/bobinados/Repository/UsuarioRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bobinator.bobinados.Repository;
 
 import bobinator.bobinados.Entity.Usuario;
@@ -10,10 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-/**
- *
- * @author groxa
- */
 public interface UsuarioRepository  extends JpaRepository<Usuario,String> {
     @Query("Select u from Usuario u where u.username = :pepe")
     public Usuario buscarUsuarioPorUsername(@Param("pepe") String usuario);
