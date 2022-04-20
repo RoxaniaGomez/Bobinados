a491cdfd2b48add386a30e166a2ffc5444af8886
Ana Emilia Ocaranza <anaocaranza4@gmail.com>
2022-04-20 19:41:39 -0300

    guardo cambios

--- src/main/java/bobinator/bobinados/Entity/CalculoDiametro.java
+++ src/main/java/bobinator/bobinados/Entity/CalculoDiametro.java
@@ -18,20 +18,10 @@ public class CalculoDiametro {
     private Double diametro;
 
     private Double seccion;
-
+    
     private Double A2;
-
-    private Double A3;
 
     private Double A5;
 
     private Double A7;
-
-    private Double A10;
-
-    private Double A15;
-
-    private Double A20;
-
-    private Double A25;
 }
