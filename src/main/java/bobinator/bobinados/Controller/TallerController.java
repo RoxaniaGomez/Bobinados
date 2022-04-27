f3dea865ee470481ad2b512ef875e46fa4417458
Federico Nickler <fedenickler@gmail>
2022-04-27 20:14:31 -0300

    cambios seguridad

--- src/main/java/bobinator/bobinados/Controller/TallerController.java
+++ src/main/java/bobinator/bobinados/Controller/TallerController.java
@@ -1,5 +1,6 @@
 package bobinator.bobinados.Controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/taller")
 public class TallerController {
 
+    @PreAuthorize("hasAnyRole('ROLE_TALLER')")
     @GetMapping("")
     public String postLogueo() {
-	return "taller";
+        return "taller";
     }
 }
