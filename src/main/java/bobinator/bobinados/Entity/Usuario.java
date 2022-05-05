2395352e52d74be5775441402066d4cf5bf5f046
Federico Nickler <fedenickler@gmail>
2022-05-05 00:22:12 -0300

    Cambios en templates

--- src/main/java/bobinator/bobinados/Entity/Usuario.java
+++ src/main/java/bobinator/bobinados/Entity/Usuario.java
@@ -1,67 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bobinator.bobinados.Entity;
 
 import bobinator.bobinados.Enum.Rol;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
-/**
- *
- * @author groxa
- */
+//@Entity
+@DiscriminatorColumn(name="tipo")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Entity
-public class Usuario {
-     @Id
+@Data
+abstract public class Usuario {
+//
+    @Id
     @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid" , strategy = "uuid2")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+
     private String id;
     
+    private String name;
     private String username;
     private String password;
+    
     @Enumerated(EnumType.STRING)
     private Rol rol;
 
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-    
-    
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    
-  
 }
--- src/main/java/bobinator/bobinados/Enum/Rol.java
+++ src/main/java/bobinator/bobinados/Enum/Rol.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bobinator.bobinados.Enum;
 
-/**
- *
- * @author groxa
- */
 public enum Rol {
-        USUARIO, ADMIN;
-    
+        TALLER, USUARIO, ADMIN;
+
 }
