23c2613c3b6de2f52da1f1b304a2650d4c2f2815
roxania <groxania@gmail.com>
2022-05-09 20:03:08 -0300

    agregando boton ver datos

--- src/main/java/bobinator/bobinados/Service/CalculoService.java
+++ src/main/java/bobinator/bobinados/Service/CalculoService.java
@@ -79,6 +79,49 @@ public class CalculoService {
         } else {
             //sera por exentrico fraccionario
            //falta Exentrico Fraccionario;
+           if (trifasico.getPolo().equals("POLO")) {
+                //por polo
+                calculo.setGrupos(trifasico.getNumeroDePolos() * 3);
+                calculo.setNumeroDeBobinaPorGrupo((trifasico.getNumeroDeRanuras() / (2 * trifasico.getNumeroDePolos() * 3)));
+                calculo.setAmplitudDeGrupo(4 * calculo.getNumeroDeBobinaPorGrupo());
+                // lista  
+                Integer y = (trifasico.getNumeroDeRanuras() / (3 * (trifasico.getNumeroDePolos() / 2)));
+                calculo.setPaso1(1);
+                calculo.setPaso2(1+y);
+                calculo.setPaso3(1+y+y);
+ 
+                
+            } else {
+                //por polo consecuente
+                calculo.setGrupos((2 / trifasico.getNumeroDePolos()) * 3);
+                calculo.setNumeroDeBobinaPorGrupo((trifasico.getNumeroDeRanuras() / (trifasico.getNumeroDePolos() * 3)));
+                calculo.setAmplitudDeGrupo(4 * calculo.getNumeroDeBobinaPorGrupo());
+                // lista
+                Integer y = (trifasico.getNumeroDeRanuras() / (3 * (trifasico.getNumeroDePolos() / 2)));
+                calculo.setPaso1(1);
+                calculo.setPaso2(1+y);
+                calculo.setPaso3(1+y+y);
+                
+            }
+         calculo.setVueltasPorBobinas((1.44 * trifasico.getNumeroDePolos() * 220) / (2 * 50 * (trifasico.getApilamientoDChapa() / 1000)
+                        * (trifasico.getDInternoDEstator() / 1000) * trifasico.getNumeroDeRanuras() * 0.8));
+                if (trifasico.getConexion().equals("ESTRELLA")) {
+                    if (trifasico.getPotenciaEnHP() <= 10) {
+                        calculo.setSeccionDelAlambre((trifasico.getCorrienteEnAmp() * 1.73) / 7 * 1.73);
+                    } else {
+                        calculo.setSeccionDelAlambre((trifasico.getCorrienteEnAmp() * 1.73) / 5 * 1.73);
+                    }
+
+                } else {
+                    //triangulo
+                    if (trifasico.getPotenciaEnHP() <= 10) {
+                        calculo.setSeccionDelAlambre((trifasico.getCorrienteEnAmp()) / 7 * 1.73);
+                    } else {
+                        calculo.setSeccionDelAlambre((trifasico.getCorrienteEnAmp()) / 5 * 1.73);
+                    }
+                }
+                calculo.setDiametroDelAlmbreMM(sqrt(4 * calculo.getSeccionDelAlambre() / PI));
+                calculo.setPesoDelAlambreKG((0.00000896*PI*(calculo.getDiametroDelAlmbreMM()*calculo.getDiametroDelAlmbreMM())/4*((((trifasico.getDInternoDEstator() * PI) / trifasico.getNumeroDePolos()) * 2 + (2 * trifasico.getApilamientoDChapa())) * calculo.getVueltasPorBobinas() * calculo.getGrupos() * calculo.getNumeroDeBobinaPorGrupo()))*1.5);
         }
         return calculoRepository.save(calculo);
     }
